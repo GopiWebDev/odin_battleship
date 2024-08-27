b92dc5362004dc2d2954afdff89b7675e5977630
GopiWebDev <gopisv.webdev@gmail.com>
2024-08-27 17:43:28 +0530

    created player and gameboard class

--- src/classes/gameBoard.js
+++ src/classes/gameBoard.js
@@ -1,5 +1,21 @@
 import Ship from './ship';
 
 class GameBoard {
-  constructor() {}
+  constructor() {
+    this.ship = {
+      carrier: new Ship(5),
+    };
+    this.missedAttacks = [];
+  }
+
+  receiveAttack(coordinates) {}
 }
+
+// need a 10x10 board
+// 5 types of ships
+// carrier takes    - > 5 spaces
+// battleship takes - > 4 spaces
+// cruiser takes    - > 3 spaces
+// submarine takes  - > 3 spaces
+// destroyer takes  - > 2 spaces
+// ships can't overlap
