090957eb3202c38397f1c56cd33c8248f3d36126
GopiWebDev <gopisv.webdev@gmail.com>
2024-08-27 16:20:06 +0530

    configued webpack, jest and added basic ship class and tested it

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Ship from './classes/ship';
+
+const ship = new Ship(5);
+console.log(ship);
---
+++ src/tests/ship.test.js
@@ -0,0 +1,16 @@
+import Ship from '../classes/ship';
+
+describe('testing the ship class', () => {
+  it('first ship length should be 5', () => {
+    const newShip = new Ship(5);
+    expect(newShip.length).toBe(5);
+  });
+
+  it('should return sunk true after 3 hits', () => {
+    const newShip = new Ship(3);
+    newShip.hit();
+    newShip.hit();
+    newShip.hit();
+    expect(newShip.isSunk()).toBe(true);
+  });
+});
